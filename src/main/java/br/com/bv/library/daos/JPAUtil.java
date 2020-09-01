5f4c6d90bc1938e06abc3a64a6f95c1f35df10d1
Bruno Vitalino <brunovitalino@hotmail.com>
2020-09-01 20:16:30 -0300

    Added list and save functionality for Autor and Obra

--- src/main/java/br/com/bv/library/daos/JPAUtil.java
+++ src/main/java/br/com/bv/library/daos/JPAUtil.java
@@ -7,8 +7,8 @@ import javax.persistence.Persistence;
 public class JPAUtil {
 
 	private static EntityManagerFactory emf = Persistence
-//			.createEntityManagerFactory("db_dev");
-			.createEntityManagerFactory("db_prod");
+//			.createEntityManagerFactory("db_postgres");
+			.createEntityManagerFactory("db_postgres_heroku");
 
 	public EntityManager getEntityManager() {
 		return emf.createEntityManager();
